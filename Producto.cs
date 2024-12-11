f94b400636645b35e21d449cceccde58ab08e4a7
JOSE ELIEZER LOPEZ FELIZ <2024-0039@unad.edu.do>
2024-12-11 18:08:19 -0400

    Update Producto.cs

--- Producto.cs
+++ Producto.cs
@@ -1,14 +1,14 @@
+<userStyle>Normal</userStyle>
+
 namespace TiendaEnLinea.Modelos
 {
     public class Producto
     {
-        // Propiedades privadas
         private string _nombre;
         private string _descripcion;
         private decimal _precio;
         private int _cantidadStock;
 
-        // Propiedad pública para el nombre
         public string Nombre
         {
             get => _nombre;
@@ -20,36 +20,37 @@ namespace TiendaEnLinea.Modelos
             }
         }
 
-        // Propiedad pública para la descripción
         public string Descripcion
         {
             get => _descripcion;
             set { _descripcion = value; }
         }
 
-        // Propiedad pública para el precio
         public decimal Precio
         {
             get => _precio;
-            set { _precio = value; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("El precio no puede ser negativo.");
+                _precio = value;
+            }
         }
 
-        // Propiedad pública para la cantidad en stock
         public int CantidadStock
         {
             get => _cantidadStock;
-            set { _cantidadStock = value; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("La cantidad en stock no puede ser negativa.");
+                _cantidadStock = value;
+            }
         }
 
-        // Método público para agregar el producto al carrito
         public void AgregarAlCarrito()
         {
-            // Lógica para agregar el producto al carrito de compras
             Console.WriteLine($"Se ha agregado {Nombre} al carrito de compras.");
         }
     }
 }
-
-git remote add origin https://github.com/CindyPaola15/Grupo-D-POO-.git
-git branch -M main
-git push -u origin main
